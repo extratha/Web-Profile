cab906d7567382584902c1c36402b9a9ae993350
Thakorn Thongrak <thakorn@edvisory.co.th>
2021-02-25 15:14:27 +0700

    add firebase db

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import Nav1 from './Nav1';
-import Intro from './Intro';
-import Profile from './Profile';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Education from './Education';
-import Experience from './Experience';
-import Skills from './Skills';
-import Contact from './Contact';
+import React, { Component } from "react";
+import "./App.css";
+import Nav1 from "./Nav1";
+import Intro from "./Intro";
+import Profile from "./Profile";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Education from "./Education";
+import Experience from "./Experience";
+import Skills from "./Skills";
+import Contact from "./Contact";
+import firebase from "firebase";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      _overlay : 'overlay backblur'
-    }
-  }
+      _overlay: "overlay backblur",
+    };
+    var firebaseConfig = {};
+    firebase.initializeApp(firebaseConfig);
+    console.log(firebase);
+  };
   callbackFunction = (overlayFromNav) => {
-    this.setState({_overlay: overlayFromNav})
-  }
+    this.setState({ _overlay: overlayFromNav });
+  };
   render() {
     return (
       <div className="main-wrap">
         <div className={this.state._overlay}></div>
-        <Nav1 parentOverlay={this.state._overlay} parentCallback={this.callbackFunction}></Nav1>
-        <Intro ></Intro>
-        <Profile  ></Profile>
-        <Education  ></Education>
-        <Experience  ></Experience>
-        <Skills  ></Skills>
-        <Contact ></Contact>
-
+        <Nav1
+          parentOverlay={this.state._overlay}
+          parentCallback={this.callbackFunction}
+        ></Nav1>
+        <Intro></Intro>
+        <Profile></Profile>
+        <Education></Education>
+        <Experience></Experience>
+        <Skills></Skills>
+        <Contact></Contact>
       </div>
-    )
-
+    );
   }
-
 }
 
 export default App;
